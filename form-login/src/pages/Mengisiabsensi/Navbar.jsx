28583d5abe1304e25538d2ff452fafe33f931e14
IlhamKP <ilhamkhairuddin52@gmail.com>
2025-12-21 14:35:09 +0800

    perbaiki button

--- form-login/src/pages/Mengisiabsensi/Navbar.jsx
+++ form-login/src/pages/Mengisiabsensi/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import "./navbar.css";
 import "./buttonmenu.css";
 import logo from "../../componnets/assest/logo.png";
 import Logoutbutton from "./Logoutbutton";
 
 function Navbar({ onLogout, setDashboardPage }) {
+
+  const [active, setActive] = useState("mengisi"); // default aktif
+    // fungsi untuk ganti halaman + set active
+  const handleClick = (page) => {
+    setDashboardPage(page); // ganti halaman utama
+    setActive(page);        // set button aktif
+  };
+
   return (
     <div className="sidebar1">
       <img src={logo} alt="logo" width={100} />
@@ -12,17 +21,27 @@ function Navbar({ onLogout, setDashboardPage }) {
         Absensi <br /> Mengaji
       </p>
 
-      <button className='menubutton' onClick={() => setDashboardPage("mengisi")}>
+      <button 
+        className={active === "mengisi" ? "menubutton active" : "menubutton"} 
+        onClick={() => handleClick("mengisi")}
+      >
         Mengisi Absensi
       </button>
 
-      <button className='menubutton' onClick={() => setDashboardPage("rekap")}>
+      <button 
+        className={active === "rekap" ? "menubutton active" : "menubutton"} 
+        onClick={() => handleClick("rekap")}
+      >
         Rekap Absensi
       </button>
 
-      <button className='menubutton' onClick={() => setDashboardPage("murid")}>
+      <button 
+        className={active === "murid" ? "menubutton active" : "menubutton"} 
+        onClick={() => handleClick("murid")}
+      >
         Cek Data Murid
       </button>
+
       <Logoutbutton onLogout={onLogout} />
     </div>
 
